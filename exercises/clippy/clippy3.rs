997ba5210200efc2c29df913936ead2ff5b82b39
XuZicang <xuzicang@stu.pku.edu.cn>
2022-11-03 19:12:20 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -6,8 +6,9 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-
-    my_option.unwrap();
+    if let Some(..) = my_option {
+        my_option.unwrap();
+    }
 
     let my_arr = &[
         -1, -2, -3,
